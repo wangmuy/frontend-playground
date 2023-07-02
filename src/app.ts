bdf670f4909cd3396c7cdbcc283944403611ebfe
wangmuy <wangmuy@gmail.com>
2023-07-02 09:45:43 +0800

    langchainjs test: yarn initial commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { HttpsProxyAgent } from "https-proxy-agent";
+import { OpenAI } from "langchain/llms/openai";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// https://github.com/openai/openai-node/issues/85
+// https://github.com/hwchase17/langchainjs/issues/1454
+const proxy = new HttpsProxyAgent("http://127.0.0.1:1091")
+export const run = async () => {
+  const model = new OpenAI(
+    {temperature: 0.9},
+    {
+      baseOptions: {proxy: false, httpAgent: proxy, httpsAgent: proxy}
+    }
+  );
+  const res = await model.call("What would be a good company name a company that makes colorful socks?")
+  console.log({res})
+};
+
+run();
